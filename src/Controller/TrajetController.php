0eb54afecc9139731657e3f502303d25a0de4416
RedNeath <theandre.bourry@gmail.com>
2023-05-29 19:07:51 +0200

    [FEAT] Recherche trajets

--- src/Controller/TrajetController.php
+++ src/Controller/TrajetController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Trajet;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,11 +25,71 @@ class TrajetController extends AbstractController
             case "fr":
                 $nextLocale = "en";
                 break;
+
+            default:
+                $nextLocale = "";
         }
 
         return $this->render('trajet/index.html.twig', [
             'ads' => $ads,
             'nextLocale' => $nextLocale
         ]);
+    }
+
+    /**
+     * @Route("/search/{_locale}/", name="trajets.search")
+     */
+    public function search(string $_locale): Response {
+
+        if (!isset($_GET['from']) ||
+            !isset($_GET['to']) ||
+            !isset($_GET['on']) ||
+            !isset($_GET['with'])
+        ) {
+            return $this->redirectToRoute('app_trajet');
+        }
+
+
+        $ads = $this->getDoctrine()->getRepository(Trajet::class)->search(
+            $_GET['from'],
+            $_GET['to'],
+            new DateTime($_GET['on']),
+            (int) $_GET['with']
+        );
+
+        switch ($_locale) {
+            case "en":
+                $nextLocale = "fr";
+                break;
+
+            case "fr":
+                $nextLocale = "en";
+                break;
+
+            default:
+                $nextLocale = "";
+        }
+
+        return $this->render('trajet/search.html.twig', [
+            'ads' => $ads,
+            'from' => $_GET['from'],
+            'to' => $_GET['to'],
+            'on' => new DateTime($_GET['on']),
+            'nextLocale' => $nextLocale
+        ]);
+    }
+
+    /**
+     * @Route("/search/", name="search.localeless")
+     */
+    public function localeless_search(): Response {
+        return $this->render('redirect_to_default_locale.html.twig');
+    }
+
+    /**
+     * @Route("/", name="localeless")
+     */
+    public function localeless_index(): Response {
+        return $this->render('redirect_to_default_locale.html.twig');
     }
 }
